47d96fc7c497959de71bb24988838f72461486a5
sohamPatkar <spatkar398@gmail.com>
2025-06-12 23:46:40 +0530

    Added UI Design and some state changes

--- Assets/Scripts/Chest/States/UnlockingState.cs
+++ Assets/Scripts/Chest/States/UnlockingState.cs
@@ -39,7 +39,7 @@ namespace ChestSystem.Chest
 
         public void OnExitState()
         {
-            GameService.Instance.ChestService.RemoveChestFromActiveChest(Owner);
+
         }
 
         public void Update()
